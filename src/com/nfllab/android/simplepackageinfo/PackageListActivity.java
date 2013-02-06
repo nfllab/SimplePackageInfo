f65a9dd03b294a72f969731c6f58e72bb881578d
Nagy Ferenc László <nflgithub@nfllab.com>
2013-02-06 01:05:32 +0100

    Initial commit

---
+++ src/com/nfllab/android/simplepackageinfo/PackageListActivity.java
@@ -0,0 +1,39 @@
+package com.nfllab.android.simplepackageinfo;
+
+import java.util.List;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class PackageListActivity extends ListActivity {
+
+	private List<PackageInfo> packages;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		PackageManager pm = getPackageManager();
+		packages = pm
+				.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
+		String[] values = new String[packages.size()];
+		for (int i = 0; i < packages.size(); i++) {
+			values[i] = packages.get(i).packageName;
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, values);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Intent intent = new Intent(this, PackageDetailsActivity.class);
+		intent.putExtra("package", packages.get(position).packageName);
+		startActivity(intent);
+	}
+}
