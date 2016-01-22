ebea674295ddaf5469bb01f1d4252625a14107ba
Nagy Ferenc László <nfl@nfllab.com>
2016-01-22 22:46:30 +0100

    cosmetic changes

--- app/src/main/java/com/nfllab/android/simplepackageinfo/PackageDetailsActivity.java
+++ app/src/main/java/com/nfllab/android/simplepackageinfo/PackageDetailsActivity.java
@@ -1,8 +1,7 @@
 package com.nfllab.android.simplepackageinfo;
 
-import android.os.Build;
-import android.os.Bundle;
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -10,6 +9,8 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.Signature;
+import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -22,74 +23,77 @@ import java.security.cert.X509Certificate;
 
 public class PackageDetailsActivity extends Activity {
 
-	@SuppressLint("NewApi")
-	private void SetupActionBar() {
-		// Show the Up button in the action bar.
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-			getActionBar().setDisplayHomeAsUpEnabled(true);
-		}
-	}
+    @SuppressLint("NewApi")
+    private void SetupActionBar() {
+        // Show the Up button in the action bar.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar ab = getActionBar();
+            if (ab != null) {
+                ab.setDisplayHomeAsUpEnabled(true);
+            }
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_package_details);
-		SetupActionBar();
-		try {
-			Intent intent = getIntent();
-			String packageName = intent.getStringExtra("package");
-			PackageManager pm = getPackageManager();
-			PackageInfo packageInfo = pm.getPackageInfo(packageName,
-					PackageManager.GET_UNINSTALLED_PACKAGES
-							| PackageManager.GET_SIGNATURES);
-			ApplicationInfo applicationInfo = packageInfo.applicationInfo;
-			TextView tv = (TextView) findViewById(R.id.textPackageName);
-			tv.setText(packageName);
-			tv = (TextView) findViewById(R.id.textVersionCode);
-			tv.setText(Integer.toString(packageInfo.versionCode));
-			tv = (TextView) findViewById(R.id.textVersionName);
-			tv.setText(packageInfo.versionName);
-			tv = (TextView) findViewById(R.id.textSourceDir);
-			tv.setText(applicationInfo.sourceDir);
-			tv = (TextView) findViewById(R.id.textPublicSourceDir);
-			tv.setText(applicationInfo.publicSourceDir);
-			Signature[] signatures = packageInfo.signatures;
-			tv = (TextView) findViewById(R.id.textNumberOfSignatures);
-			tv.setText(Integer.toString(signatures.length));
-			CertificateFactory X509Factory = CertificateFactory
-					.getInstance("X509");
-			X509Certificate cert = (X509Certificate) X509Factory
-					.generateCertificate(new ByteArrayInputStream(signatures[0]
-							.toByteArray()));
-			tv = (TextView) findViewById(R.id.textSignature);
-			tv.setText(cert.toString());
-		} catch (NullPointerException e) {
-			Toast.makeText(this, "NullPointerException", Toast.LENGTH_LONG)
-					.show();
-		} catch (NameNotFoundException e) {
-			Toast.makeText(this, "NameNotFoundException", Toast.LENGTH_LONG)
-					.show();
-		} catch (CertificateException e) {
-			Toast.makeText(this, "CertificateException", Toast.LENGTH_LONG)
-					.show();
-		}
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_details);
+        SetupActionBar();
+        try {
+            Intent intent = getIntent();
+            String packageName = intent.getStringExtra("package");
+            PackageManager pm = getPackageManager();
+            PackageInfo packageInfo = pm.getPackageInfo(packageName,
+                    PackageManager.GET_UNINSTALLED_PACKAGES
+                            | PackageManager.GET_SIGNATURES);
+            ApplicationInfo applicationInfo = packageInfo.applicationInfo;
+            TextView tv = (TextView) findViewById(R.id.textPackageName);
+            tv.setText(packageName);
+            tv = (TextView) findViewById(R.id.textVersionCode);
+            tv.setText(Integer.toString(packageInfo.versionCode));
+            tv = (TextView) findViewById(R.id.textVersionName);
+            tv.setText(packageInfo.versionName);
+            tv = (TextView) findViewById(R.id.textSourceDir);
+            tv.setText(applicationInfo.sourceDir);
+            tv = (TextView) findViewById(R.id.textPublicSourceDir);
+            tv.setText(applicationInfo.publicSourceDir);
+            Signature[] signatures = packageInfo.signatures;
+            tv = (TextView) findViewById(R.id.textNumberOfSignatures);
+            tv.setText(Integer.toString(signatures.length));
+            CertificateFactory X509Factory = CertificateFactory
+                    .getInstance("X509");
+            X509Certificate cert = (X509Certificate) X509Factory
+                    .generateCertificate(new ByteArrayInputStream(signatures[0]
+                            .toByteArray()));
+            tv = (TextView) findViewById(R.id.textSignature);
+            tv.setText(cert.toString());
+        } catch (NullPointerException e) {
+            Toast.makeText(this, "NullPointerException", Toast.LENGTH_LONG)
+                    .show();
+        } catch (NameNotFoundException e) {
+            Toast.makeText(this, "NameNotFoundException", Toast.LENGTH_LONG)
+                    .show();
+        } catch (CertificateException e) {
+            Toast.makeText(this, "CertificateException", Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
 }
--- app/src/main/java/com/nfllab/android/simplepackageinfo/PackageListActivity.java
+++ app/src/main/java/com/nfllab/android/simplepackageinfo/PackageListActivity.java
@@ -1,44 +1,44 @@
 package com.nfllab.android.simplepackageinfo;
 
-import java.util.Arrays;
-import java.util.List;
-
-import android.os.Bundle;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PackageListActivity extends ListActivity {
 
-	private List<PackageInfo> packages;
+    private List<PackageInfo> packages;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		PackageManager pm = getPackageManager();
-		packages = pm
-				.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
-		String[] values = new String[packages.size()];
-		for (int i = 0; i < packages.size(); i++) {
-			values[i] = packages.get(i).packageName;
-		}
-		// the package manager used to return the packages in alphabetical order,
-		// but it was changed some time ago
-		Arrays.sort(values);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, values);
-		setListAdapter(adapter);
-		getListView().setFastScrollEnabled(true);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PackageManager pm = getPackageManager();
+        packages = pm
+                .getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
+        String[] values = new String[packages.size()];
+        for (int i = 0; i < packages.size(); i++) {
+            values[i] = packages.get(i).packageName;
+        }
+        // the package manager used to return the packages in alphabetical order,
+        // but it was changed some time ago
+        Arrays.sort(values);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+        getListView().setFastScrollEnabled(true);
+    }
 
-	@Override
-	protected void onListItemClick(ListView l, View v, int position, long id) {
-		Intent intent = new Intent(this, PackageDetailsActivity.class);
-		intent.putExtra("package", packages.get(position).packageName);
-		startActivity(intent);
-	}
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(this, PackageDetailsActivity.class);
+        intent.putExtra("package", packages.get(position).packageName);
+        startActivity(intent);
+    }
 }
