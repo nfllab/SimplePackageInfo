cc5d5153c8aa5a37bc8870c7a019b80060686009
Nagy Ferenc László <nfl@nfllab.com>
2016-01-23 23:20:05 +0100

    remove v4 support library

--- app/src/main/java/com/nfllab/android/simplepackageinfo/PackageDetailsActivity.java
+++ app/src/main/java/com/nfllab/android/simplepackageinfo/PackageDetailsActivity.java
@@ -1,6 +1,5 @@
 package com.nfllab.android.simplepackageinfo;
 
-import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
@@ -11,7 +10,6 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +21,6 @@ import java.security.cert.X509Certificate;
 
 public class PackageDetailsActivity extends Activity {
 
-    @SuppressLint("NewApi")
     private void SetupActionBar() {
         // Show the Up button in the action bar.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -83,14 +80,11 @@ public class PackageDetailsActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpFromSameTask(this);
+                /* This should go back to the already running parent activity, without restarting
+                 * it. We don't want the filter cleared. */
+                Intent upIntent = new Intent(this, PackageListActivity.class);
+                upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(upIntent);
                 return true;
         }
         return super.onOptionsItemSelected(item);
